fc91cf457e2612ecc5a896dada6ae9e31ec8d355
iam-junedali <junedali.sk@gmail.com>
2025-03-14 11:04:52 +0530

    updated the files and commands

---
+++ Shell_Scripting/frontend_server.sh
@@ -0,0 +1,8 @@
+dnf install nginx -y
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+systemctl enable nginx
+systemctl restart nginx
