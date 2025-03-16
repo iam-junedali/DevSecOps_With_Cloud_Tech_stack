85e04fc986937a9ad165c483f1b62fefe6486a25
iam-junedali <junedali.sk@gmail.com>
2025-03-16 11:01:18 +0530

    Enabled the messages with clours for better productivity and redirect the log files.

--- Shell_Scripting/frontend_server.sh
+++ Shell_Scripting/frontend_server.sh
@@ -1,14 +1,18 @@
-dnf install nginx -y
+echo -e "\e[33m Install Nginx server\e[0m"
+dnf install nginx -y >/tmp/project.log
 
-systemctl enable nginx
-systemctl start nginx
+echo -e "\e[33m Remove the default content\e[0m"
+rm -rf /usr/share/nginx/html/* >/tmp/project.log
 
-rm -rf /usr/share/nginx/html/*
+echo -e "\e[33m Download the Frontend roboshop content\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/project.log
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-
+echo -e "\e[33m Navigate to Nginx path\e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >/tmp/project.log
 
-systemctl enable nginx
-systemctl restart nginx
+echo -e "\e[33m Enable the Nginx server\e[0m"
+systemctl enable nginx >/tmp/project.log
+
+echo -e "\e[33m Restart the Nginx server\e[0m"
+systemctl restart nginx >/tmp/project.log
