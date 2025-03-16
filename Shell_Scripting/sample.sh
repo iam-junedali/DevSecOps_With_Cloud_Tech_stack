8839d8833b5ab3d07ac225b03a405734d4aeaf0d
iam-junedali <junedali.sk@gmail.com>
2025-03-16 10:47:16 +0530

    updated the files and commands

--- Shell_Scripting/sample.sh
+++ Shell_Scripting/sample.sh
@@ -1,2 +1,8 @@
 ls
 whoami
+echo -e "/e[31m Hey this is one more colour/e[0m"
+echo -e "/e[32m Hey this is one more colour/e[0m"
+echo -e "/e[33m Hey this is one more colour/e[0m"
+echo -e "/e[34m Hey this is one more colour/e[0m"
+echo -e "/e[35m Hey this is one more colour/e[0m"
+echo -e "/e[36m Hey this is one more colour/e[0m"
