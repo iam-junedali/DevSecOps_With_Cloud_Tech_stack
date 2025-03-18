4d303f6c8fc0829f345fef014fd83a4590d91129
iam-junedali <junedali.sk@gmail.com>
2025-03-18 10:45:25 +0530

    Enabled the messages with clours for better productivity and redirect the log files.

--- Shell_Scripting/frontend_server.sh
+++ Shell_Scripting/frontend_server.sh
@@ -1,18 +1,18 @@
-echo -e "\e[33m Install Nginx server\e[0m"
-dnf install nginx -y >/tmp/project.log
+echo -e "\e[33m Installing the Nginx server\e[0m"
+dnf install nginx -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m Remove the default content\e[0m"
-rm -rf /usr/share/nginx/html/* >/tmp/project.log
+echo -e "\e[33m Remove the default hosting content\e[0m"
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
-echo -e "\e[33m Download the Frontend roboshop content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/project.log
+echo -e "\e[33m Downloading the Frontend content of roboshop\e[0m"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[33m Navigate to Nginx path\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip >/tmp/project.log
+echo -e "\e[33m Unzipping the hosting content of Nginx server\e[0m"
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[33m Enable the Nginx server\e[0m"
-systemctl enable nginx >/tmp/project.log
+echo -e "\e[33m Enabling the Nginx server\e[0m"
+systemctl enable nginx &>>/tmp/roboshop.log
 
-echo -e "\e[33m Restart the Nginx server\e[0m"
-systemctl restart nginx >/tmp/project.log
+echo -e "\e[33m Restarting the Nginx server\e[0m"
+systemctl restart nginx &>>/tmp/roboshop.log
